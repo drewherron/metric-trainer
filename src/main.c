c03f5dd384e6dbdb63ca44d240488f45e8da3eb6
Drew Herron <herron@mailbox.org>
2023-08-14 22:17:30 -0700

    Improve menu

--- src/main.c
+++ src/main.c
@@ -3,15 +3,17 @@
 #include "questions.h"
 
 void show_menu(void) {
+    printf("\n");
     printf("Metric Trainer - Metric Conversion Practice\n");
     printf("==========================================\n\n");
     printf("Select categories:\n");
-    printf("a) Distance\n");
-    printf("b) Weight\n");
-    printf("c) Temperature\n");
-    printf("d) Volume\n");
-    printf("all) All categories\n\n");
+    printf("  a) Distance     (miles ↔ km, inches ↔ cm)\n");
+    printf("  b) Weight       (pounds ↔ kg)\n");
+    printf("  c) Temperature  (Celsius ↔ Fahrenheit)\n");
+    printf("  d) Volume       (gallons ↔ liters)\n");
+    printf("  all) All categories\n\n");
     printf("Enter choice (e.g., \"b\", \"all\", or \"ac\"): ");
+    fflush(stdout);
 }
 
 int main(void) {
